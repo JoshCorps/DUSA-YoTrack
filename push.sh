67edb2958a758b8597fb1ec47086afdb96308c97
Josh2609 <jcorps@gmx.com>
2017-09-11 12:57:57 +0000

    Requirements

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Usage: sh push.sh "Commit title here"
+# (if you are using somebody elses github info, add your name in brackets at the start of your commit title)
 
 git add .
 git commit -m "$1"
