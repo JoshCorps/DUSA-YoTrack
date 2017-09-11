109f2bfd51686e3eaa36f6c2d547434fb03531ee
Josh2609 <jcorps@gmx.com>
2017-09-11 14:33:54 +0000

    Pushing with own email

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Usage: sh push.sh "Commit title here"
+# Usage: sh push.sh "youremail@here" "Commit title here"
 # (if you are using somebody elses github info, add your name in brackets at the start of your commit title)
-
+git config user.email "$1"
 git add .
-git commit -m "$1"
+git commit -m "$2"
 git push -u origin master 
