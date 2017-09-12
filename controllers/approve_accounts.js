6d843e30cf27eae1f1a886b20c477a3e9d4d0b8b
Josh2609 <jcorps@gmx.com>
2017-09-12 15:37:19 +0000

    Approve accounts page base done, not tested

--- controllers/approve_accounts.js
+++ controllers/approve_accounts.js
@@ -2,13 +2,39 @@
 
 let express = require('express');
 let router = express.Router();
+let User = require('../models/user.js');
+let bodyParser = require('body-parser');
 
-router.get('/', (request, response) => {
-  
+express.use(bodyParser.urlencoded({extended: true}));
+
+router.get('/', (req, res) => {
+  User.getUnapprovedUsers((err, users) => {
+      if(err) {
+          // handle error somehow
+      }
+      
+      res.render('approve_accounts', {
+          users: users
+      });
+  });
 });
 
-router.post('/', (request, response) => {
-    
+router.post('/', (req, res) => {
+    var approved, declined;
+    var users = Array.prototype.slice.call(req.body.users);
+    for(let i=0; i<users.length; i++) {
+        if (users[i].isApproved) {
+            approved.push(users[i].email);
+        } else {
+            declined.push(users[i].email);
+        }
+    }
+    if (approved.length != 0) {
+        User.approveUsers(approved);
+    }
+    if (declined.length != 0) {
+        User.deleteUsersByEmails(declined);
+    }
 });
 
 module.exports = router;
