c818d446fc95aa46ca397c18d64f937b4b437b4f
Josh2609 <jcorps@gmx.com>
2017-09-12 15:45:12 +0000

    Basic registration done, not tested.

--- controllers/register.js
+++ controllers/register.js
@@ -3,8 +3,49 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (request, response) => {
+//models
+let db = require('../models/db.js')();
+let User = require('../models/user.js');
 
+router.get('/', (request, response) => {
+    response.render('register.pug');
+});
+
+router.post('/', (request, reponse) => {
+    let firstName = request.body.firstName;
+    let lastName = request.body.lastName;
+    let email = request.body.email;
+    let password = request.body.password;
+    let repeatpassword = request.body.repeatpassword;
+    /*
+    if (password !== repeatpassword)
+    {
+        // return error 
+    }
+    if (!email.contains("@dusa") || !email.contains("@dundee"))
+    {
+        // return invalid email
+    }
+    if (password.length < 8)
+    {
+        //pass too short
+    }
+    */
+    
+    var hashedPassword = test//User.setPassword(password, false);
+    
+    
+    var data = [
+        {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashedPassword,
+            //accountType: ,
+            isApproved: false}
+        ];
+    User.create(db, data);
+   console.log(data)     
 });
 
 module.exports = router;
